02330b4ac41e0988caa2cc5a38330e97d4ccc775
tyrty23 <108882283+tyrty23@users.noreply.github.com>
2023-02-20 11:41:21 +0300

    Add files via upload

--- Graph.cpp
+++ Graph.cpp
@@ -18,8 +18,8 @@ void Graph::AddEdge(int a, int b)
 
 void Graph::Deg_plus(int n)
 {
-	for (int i=0; i < V; i++)
-		for (auto it1=Edges[n].begin(); it1 !=Edges[n].end(); it1++) {
+	for (int i = 0; i < V; i++)
+		for (auto it1 = Edges[n].begin(); it1 != Edges[n].end(); it1++) {
 			if ((*it1) == V2[i]) {
 				deg_plus[i]++;
 			}
@@ -31,9 +31,12 @@ void Graph::Deg_minus(int n)
 	bool flag = true;
 	for (int i = 0; i < V; i++) {
 		for (auto it1 = Edges[n].begin(); it1 != Edges[n].end(); it1++) {
-			if (deg_plus[i] == 0) {
-				/*it1=Edges[n].erase(it1);*/
+			if (deg_plus[i] == 0 ) {
+				it1=Edges[n].erase(it1);				
 				flag = false;
+				if (it1 == Edges[n].end()) {
+					break;
+				}
 			}
 		}
 		if (flag == false) {
@@ -55,33 +58,68 @@ int* Graph::TopologicalSort()
 	}
 
 	while (1) {
+		cout << "\nкруг" << endl;
 
+
+		cout << "\nВыод листов\n";
+		 
+		for (int i = 0; i < V; i++) {
+			for (auto it1 = Edges[i].begin(); it1 != Edges[i].end(); it1++) {
+				cout << *it1;
+			}
+
+			cout << endl;
+		}
+
+		cout << "\nоставшиеся элементы\n";
+		for (int i = 0; i < 6; i++) {
+			cout << V2[i];
+		}
+		cout << endl;
 		for (int i = 0; i < V; i++) {
 			Deg_plus(i);
 		}
+
+		cout << endl;
+
+		for (int i = 0; i < 6; i++) {
+			cout << deg_plus[i];
+		}
+
+
 		bool flag1 = false;
 		for (int i = 0; i < V; i++) {
 			if (deg_plus[i] == 0) {
 				answer[counter] = V2[i];
 				counter++;
 				flag1 = true;
-			}		
+			}
 		}
-		if(flag1==false) {
-			cout << "Довести сортировку до конца невозможно, отсортированные элементы: ";
+		if (flag1 == false) {
+			cout << "Полностью отсортировать граф не удалось, отсортированые элементы: ";
 			for (int j = 0; j < counter; j++) {
 				cout << answer[j];
 			}
 			return answer;
+		}
+		cout <<"\nanswer"<< endl;
+		for (int j = 0; j < counter; j++) {
+			cout << answer[j];
 		}
 
 		for (int i = 0; i < V; i++) {
 			Deg_minus(i);
 		}
 
-		for (int i = 0; i < V; i++) {
-			deg_plus[i]=0;
+		for (int i = 0; i < 6; i++) {
+			deg_plus[i] = 0;
 		}
+		cout << "\nоставшиеся элементы\n";
+		for (int i = 0; i < 6; i++) {
+			cout<<V2[i];
+		}
+		cout << endl;
+
 
 		if (counter == counter1) {
 			cout << "Граф полностью отсортирован: ";
